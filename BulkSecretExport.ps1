165df0248ef97c78552e2f71b180290a668df7e3
Shreyas <ShreyasVenkatesh@users.noreply.github.com>
2020-08-10 23:53:28 +0530

    Power shell script for Azure bulk secret export
    This is a power shell script to export selected Secrets from a given Source Azure Key Vault to Azure Destination Key Vault.

---
+++ BulkSecretExport.ps1
@@ -0,0 +1,31 @@
+﻿Param(
+[Parameter(Mandatory)]
+[string]$sourceKeyVault,
+[Parameter(Mandatory)]
+[string]$destinationKeyVault,
+[Parameter(Mandatory)]
+[string[]]$secretsList
+)
+
+#Display Secrets in New Key Vault
+Write-Host ' Checking If secrets are present in source Key Vault :' $sourceKeyVault ' to be moved to destination Key Vault :' $destinationKeyVault
+$SecretsFound = $false
+#Loop over secrets to be moved to destination key vault
+foreach($currentSecret in $secretsList)
+{
+    $sourceSecretDetails = Get-AzKeyVaultSecret -VaultName $sourceKeyVault -Name $currentSecret
+    if($sourceSecretDetails)
+    {
+        $SecretName = $sourceSecretDetails.Name
+        $secureString = ConvertTo-SecureString -String $sourceSecretDetails.SecretValueText -AsPlainText -Force  
+        Write-Host 'Found Secret in soruce Key Vault :' $sourceKeyVault 'Secret Name =' $SecretName ': Value =' $sourceSecretDetails.SecretValueText
+        Set-AzKeyVaultSecret -VaultName $destinationKeyVault -Name $SecretName -SecretValue $secureString
+        Write-Host 'Successfully added Secret in destination Key Vault :' $destinationKeyVault' Secret Name =' $SecretName
+        $SecretsFound = $true
+    }
+}
+if(!$SecretsFound)
+{
+    Write-Host 'No Secrets in Source Key Vault'
+}
+
